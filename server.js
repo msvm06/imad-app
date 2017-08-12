e62d06e090eaedf0ab71cb2f411cd69992530014
msvm06 <30391301+msvm06@users.noreply.github.com>
2017-08-12 23:16:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-pp.get('/article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile('Article one requested and will be served here');
 });
 
