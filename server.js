fd8b8a8ebd059795adb4e35501f4cf10fccede3b
msvm06 <30391301+msvm06@users.noreply.github.com>
2017-08-20 22:13:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-
+app.get('/test-db',function(req,res){
+    
+    //make a select request
+    //return a response with the result
+    
+});
 
 
 app.get('/ui/madi.png', function (req, res) {
